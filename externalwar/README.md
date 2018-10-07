a5004aeaeeddd49112224c0d5493e2be40aea760
foxneo <mcaceres@uni-bremen.de>
2018-10-07 10:54:04 +0200

    Add Spring boot converter to WAR file

---
+++ externalwar/README.md
@@ -0,0 +1,8 @@
+# Convert spring-boot application to WAR Web Application
+
+This is a simple project, it can be used as a template for a huge project. Ready to deploy on Tomcat server
+
+## Build
+```
+$ mvn clean install
+```
---
+++ externalwar/src/main/java/site/sample/externalwar/ExternalwarApplication.java
@@ -0,0 +1,23 @@
+package site.sample.externalwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 
+ * @author Miguel Caceres This Spring Boot application runs on Tomcat
+ */
+@SpringBootApplication
+public class ExternalwarApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalwarApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
+}
