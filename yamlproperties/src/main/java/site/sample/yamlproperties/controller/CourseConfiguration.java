70d459429ac7d429788e782e530ea1709d89d073
foxneo <mcaceres@uni-bremen.de>
2018-10-16 22:38:43 +0200

    Add Spring-Boot with YAML

---
+++ yamlproperties/src/main/java/site/sample/yamlproperties/controller/CourseConfiguration.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package site.sample.yamlproperties.controller;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Miguel Caceres
+ *
+ */
+@Component
+@ConfigurationProperties("course")
+public class CourseConfiguration {
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getChapterCount() {
+		return chapterCount;
+	}
+
+	public void setChapterCount(int chapterCount) {
+		this.chapterCount = chapterCount;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	private String name;
+
+	private int chapterCount;
+
+	private int rating;
+
+	private String author;
+
+}
