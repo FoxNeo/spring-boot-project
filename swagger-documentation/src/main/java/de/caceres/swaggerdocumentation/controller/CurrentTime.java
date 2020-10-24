a484d0c76be41efd1746d842ffac8c94bef07a38
Miguel Caceres <mcaceres@uni-bremen.de>
2020-10-24 18:07:31 +0200

    Add Swagger API Documentation Dashboard

---
+++ swagger-documentation/src/main/java/de/caceres/swaggerdocumentation/controller/CurrentTime.java
@@ -0,0 +1,29 @@
+package de.caceres.swaggerdocumentation.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+@RestController
+public class CurrentTime {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CurrentTime.class);
+
+    private static final SimpleDateFormat formater = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+
+    @RequestMapping(value = "/time", method = RequestMethod.GET)
+    public String getCurrentTime(){
+        return generateTimestamp();
+    }
+
+    private String generateTimestamp(){
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        return formater.format(timestamp).toString();
+    }
+
+}
---
+++ swagger-documentation/src/test/java/de/caceres/swaggerdocumentation/SwaggerDocumentationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.caceres.swaggerdocumentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerDocumentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
