9f62396fef20d3e6c8a50762131998aec45e365d
FoxNeo <m.caceres@mailbox.org>
2023-09-24 14:12:44 +0200

    Migrate to Spring Boot 3 project log4jlogging

--- log4jlogging/src/test/java/bhupen/in/log4jlogging/Log4jloggingApplicationTests.java
+++ log4jlogging/src/test/java/bhupen/in/log4jlogging/Log4jloggingApplicationTests.java
@@ -1,12 +1,10 @@
 package bhupen.in.log4jlogging;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import site.sample.log4jlogging.Log4jloggingApplication;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Log4jloggingApplication.class)
 public class Log4jloggingApplicationTests {
 
 	@Test
