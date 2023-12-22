c21889311e24b8b25b816f89b6748c08a965fd85
Miguel Caceres <m.caceres@mailbox.org>
2023-12-22 23:18:32 +0100

    Fix broken build and migrate swagger to SpringBoot 3

--- swagger-documentation/src/main/java/de/caceres/swaggerdocumentation/SpringWebConfig.java
+++ swagger-documentation/src/main/java/de/caceres/swaggerdocumentation/SpringWebConfig.java
@@ -1,9 +1,7 @@
 package de.caceres.swaggerdocumentation;
 
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger
 public class SpringWebConfig {
 }
